1cb5d7f729f9c40b5530b089603d206eb7ba499b
Azizah Ibrahim <137410873+azizah-ibrahim@users.noreply.github.com>
2023-07-21 10:29:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'Django']
+  var member = {
+    name: 'John',
+    age: 23,
+    skills: skills
+  }
+  return member
+}
